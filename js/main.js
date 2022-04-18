2f560ca7dc6fcecacdb857c592223b4e3c2cc326
Scott Burton <burtonsb@gmail.com>
2022-04-18 14:56:17 -0400

    issue getting Date Obj to work

--- js/main.js
+++ js/main.js
@@ -6,16 +6,30 @@ function getFetch(){
   const long = document.querySelector('#longInput').value
   
   console.log(lat,long)
-  const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}date=today`
+  const url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}date=today&formatted=0`
 
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
+        console.log('sunrise:', data.results.sunrise)
+        console.log('sunset:', data.results.sunset)
+        
+        // this should come up as a new Date but returns an empty object
+        let sunrise = new Date(data.results.sunrise)
+        // console.log(sunrise.toLocaleTimeString())
+        console.log(sunrise)
+        // can't do any methods on the date object
 
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
+}
+
+class Date {
+  constructor(day, month, year) {
+
+  }
 }
